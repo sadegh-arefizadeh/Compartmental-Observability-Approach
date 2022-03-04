d468d011f0bc34453db4fe062de0f3988b176444
sadegh-arefizadeh <96794719+sadegh-arefizadeh@users.noreply.github.com>
2022-03-04 14:54:37 +0330

    Update evaluation_1.m

--- evaluation_1.m
+++ evaluation_1.m
@@ -8,6 +8,8 @@ load('welfare_6to9_3.mat')
 percentage_convex=zeros(1,1000);
 percentage_improvement=zeros(1,1000);
 
+%welfares{i}{3}(end) is the maximum and welfares{i}{3}(1) is minimum welfare
+
 for i=1:1000   
    PPO_algorithm_1(i)= (1-((welfares{i}{3}(end)-welfares{i}{1})/(welfares{i}{3}(end)-welfares{i}{3}(1))));
    PPO_algorithm_2(i)=(1-((welfares{i}{3}(end)-welfares{i}{2})/(welfares{i}{3}(end)-welfares{i}{3}(1))));
