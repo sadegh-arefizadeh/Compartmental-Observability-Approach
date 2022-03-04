a8697815111a2a2a53994ea30efa35f811d2d28a
sadegh-arefizadeh <96794719+sadegh-arefizadeh@users.noreply.github.com>
2022-03-04 14:46:19 +0330

    Update evaluation_1.m

--- evaluation_1.m
+++ evaluation_1.m
@@ -2,21 +2,16 @@ clear
 clc
 
 % load('welfare_0to3_3.mat')
-load('welfare_3to6_3.mat')
-% load('welfare_6to9_3.mat')
-
+% load('welfare_3to6_3.mat')
+load('welfare_6to9_3.mat')
 
 percentage_convex=zeros(1,1000);
 percentage_improvement=zeros(1,1000);
 
-
 for i=1:1000   
-   percentage_convex(i)= (1-((welfares{i}{3}(end)-welfares{i}{1})/(welfares{i}{3}(end)-welfares{i}{3}(1))))*100;
-   percentage_improvement(i)=(1-((welfares{i}{3}(end)-welfares{i}{2})/(welfares{i}{3}(end)-welfares{i}{3}(1))))*100;
-  
+   PPO_algorithm_1(i)= (1-((welfares{i}{3}(end)-welfares{i}{1})/(welfares{i}{3}(end)-welfares{i}{3}(1))));
+   PPO_algorithm_2(i)=(1-((welfares{i}{3}(end)-welfares{i}{2})/(welfares{i}{3}(end)-welfares{i}{3}(1))));
 end
 
-
-median(percentage_convex)
-median(percentage_improvement)
-
+median(PPO_algorithm_1)
+median(PPO_algorithm_2)
